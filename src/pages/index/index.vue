8ef4724a3f76ac499bcc7efce3a6385befc401ae
1101099728@qq.com <142xyh753869>
2021-02-19 17:46:01 +0800

    加web-view

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,18 +2,48 @@
   <view class="index">
     <NumberDisplay/>
     <NumberSubmit/>
+    <button @tap="toWebView('https://daxigua-xi-jade.vercel.app')">
+      test btn
+    </button>
   </view>
 </template>
 
 <script>
 import NumberDisplay from '../../components/NumberDisplay.vue'
 import NumberSubmit from '../../components/NumberSubmit.vue'
-
+import {onMounted} from 'vue'
+import Taro from '@tarojs/taro'
 export default {
   name: 'Index',
   components: {
     NumberDisplay,
     NumberSubmit
+  },
+
+
+  // data() {
+  // },
+  // methods: {
+  //   toWebView (e) {
+  //     console.log(e)
+  //   }
+  // }
+
+
+
+  setup() {
+    const toWebView = (src) => {
+      console.log(src)
+      Taro.navigateTo({
+        url: `/pages/web-view/web-view?src=${src}`,
+      })
+    }
+    onMounted(() => {
+      console.log('onMounted00')
+    })
+    return {
+      toWebView
+    }
   }
 }
 </script>
---
+++ src/pages/web-view/web-view.vue
@@ -0,0 +1,28 @@
+<template>
+  <web-view :src='src' onMessage={this.handleMessage} ></web-view>
+</template>
+
+<script>
+
+export default {
+  name: 'WebView',
+  components: {
+  },
+  data () {
+    return {
+      src: ''
+    }
+  },
+  methods: {
+    handleMessage() {
+    }
+  },
+  onLoad (e) {
+    console.log(e)
+    this.src = e.src
+  }
+}
+</script>
+
+<style>
+</style>
